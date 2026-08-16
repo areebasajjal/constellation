9d128c56dfedbfc870a108101b714669c42ba4bf
areeba sajjal <198977893+areebasajjal@users.noreply.github.com>
2026-08-16 08:30:13 +0500

    feat; added basic backend logic to the create speaces module

--- app/Constellationspace/page.tsx
+++ app/Constellationspace/page.tsx
@@ -1,14 +1,44 @@
+"use client";
+
+import { useState } from "react";
+
 export default function SpacesPage() {
+
+  // Stores whatever the user is currently typing in the "Create a Space" input.
+  const [spaceName, setSpaceName] = useState("");
+
+  // Stores the final space name after clicks "Create space".
+  const [createdSpace, setCreatedSpace] = useState("");
+
+  function handleSpaceNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const newName = event.target.value;
+
+    setSpaceName(newName);
+  }
+
+  function handleCreateSpace() {
+
+    if (spaceName.trim() === "") {
+      return;
+    }
+
+    // Save the current typed space nameas the created space.
+    setCreatedSpace(spaceName);
+
+    setSpaceName("");
+  }
+
   return (
     <main className="space-page">
 
       <section className="space-content">
-      <div className="star star-1 star-bright"></div>
 
-      <div className="star star-2"></div>
-
-      <div className="star star-77"></div>
-
+        <div className="star star-11"></div>
+        <div className="star star-2"></div>
+        <div className="star star-77"></div>
+        <div className="star star-88"></div>
+        <div className="star star-99"></div>
+         <div className="star star-00"></div>
 
         <p className="space-kicker">
           ENTER THE CONSTELLATION
@@ -28,12 +58,15 @@ export default function SpacesPage() {
 
             <input
               type="text"
-              placeholder="e.g. GIRLIES26"/>
+              placeholder="e.g. GIRLIES26"
+            />
 
             <button>
               Join space →
             </button>
+
           </div>
+
 
           <div className="space-option">
 
@@ -45,12 +78,25 @@ export default function SpacesPage() {
 
             <input
               type="text"
+
+              // The input always shows whateveris currently stored in spaceName.
+              value={spaceName}
+
+              onChange={handleSpaceNameChange}
+
               placeholder="Name your space"
             />
 
-            <button>
+            <button onClick={handleCreateSpace}>
               Create space →
             </button>
+
+            {createdSpace && (
+              <p>
+                Space created: {createdSpace}
+              </p>
+            )}
+
           </div>
 
         </div>
