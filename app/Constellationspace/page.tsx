559035d15a60cb6b79c49aaa39effcf2029611e0
areeba sajjal <198977893+areebasajjal@users.noreply.github.com>
2026-08-16 08:46:35 +0500

    add basic feture of joining space via the code ( basic logic )

--- app/Constellationspace/page.tsx
+++ app/Constellationspace/page.tsx
@@ -9,16 +9,19 @@ export default function SpacesPage() {
 
   // Stores the final space name after clicks "Create space".
   const [createdSpace, setCreatedSpace] = useState("");
+
   const [spaceCode, setSpaceCode] = useState("");
 
   const [joinCode, setJoinCode] = useState("");
 
+  // Function to handle changes in the "Create a Space" input field.
   function handleSpaceNameChange(event: React.ChangeEvent<HTMLInputElement>) {
     const newName = event.target.value;
 
     setSpaceName(newName);
   }
 
+// function to generate a unique space code based on the space name.
   function generateSpaceCode(name: string) {
       const cleanName = name.replace(/\s+/g, "").toUpperCase();
 
@@ -26,23 +29,30 @@ export default function SpacesPage() {
 
       const randomNumber = Math.floor(100 + Math.random() * 900
   );
-  
+
   return shortName + randomNumber;
 }
 
+// fuction to handle the creation of a new space when the user clicks the "Create space" button.
   function handleCreateSpace() {
-
-    if (spaceName.trim() === "") {
-      return;
+       if (spaceName.trim() === "") {
+           return;
     }
 
     const newCode = generateSpaceCode(spaceName);
-    // Save the current typed space nameas the created space.
+    
     setCreatedSpace(spaceName);
     setSpaceCode(newCode);
 
     setSpaceName("");
   }
+
+
+   function handleJoinCodeChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const newCode = event.target.value;
+        setJoinCode(newCode);
+}
+
 
   return (
     <main className="space-page">
@@ -75,6 +85,8 @@ export default function SpacesPage() {
             <input
               type="text"
               placeholder="e.g. GIRLIES26"
+              value={joinCode}
+              onChange={handleJoinCodeChange}
             />
 
             <button>
